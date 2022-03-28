54a0e8ccdcfca14d988ef9f85f71e988e674e08b
MO EYMEN KARAM <65572032+Mhd-krm12@users.noreply.github.com>
2022-03-28 22:42:38 +0300

    Update image_dimensions.py

--- image_dimensions.py
+++ image_dimensions.py
@@ -1,3 +1,8 @@
+#EN: Get all Image path Size in folder.
+#TR: Bu kod yardımıyla bir klasör belirleyip içinde yürüyüp ve bütün resimlerin yolunu ve boyutunu yazdırabiliriz.
+#AR: يقوم هذا الكود بالتنقل داخل المجلد الذي حددناه وجمع كل الصور وطباعة مسار وأبعاد الصور.
+
+
 import cv2
 import os
 
@@ -8,3 +13,6 @@ for paths,folders,files in os.walk(os.getcwd()):
                 image_path=((os.path.join(paths,file)))
                 img=cv2.imread(image_path)
                 print("Image path:  "+str(image_path)+"\n"+"Image Size: "+str(img.shape)+"\n")
+                
+                
+                
