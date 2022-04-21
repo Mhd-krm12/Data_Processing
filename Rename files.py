3cbd9e81b9dbd446a55164e3134e5100c7b32dd3
MO EYMEN KARAM <65572032+Mhd-krm12@users.noreply.github.com>
2022-04-21 12:35:26 +0300

    Create Rename files.py

---
+++ Rename files.py
@@ -0,0 +1,14 @@
+import glob, os
+
+path=r"C:\Users\...\folder"
+def get_image_files(folder_path):
+    img_formats = ['jpg','png', 'jpeg',"txt"] 
+    files = []
+    for image_file in glob.iglob(os.path.join(folder_path, "**"), recursive=True):
+        files.append(image_file)
+    return [x for x in files if x.split('.')[-1].lower() in img_formats]
+    for i in get_image_files(path):
+        os.rename(i, str(i.split(".")[:-1][0]) + "edetid" + str(i.split(".")[-1]))
+        os.rename(i, i.replace("-", ""))
+        
+          
